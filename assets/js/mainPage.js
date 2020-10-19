c5547b7c8a08510380ac57649876360b40b4b74c
Aleksa Jukic <yukajunior007@gmail.com>
2020-10-19 09:53:39 +0200

    Add files via upload

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -1,6 +1,47 @@
 $(document).ready(function () {
 
-    // Prikaz loadera u zavinsosti od dana
+    // Animacija za side navigation
+
+    $('#triggerSideNav').click(() => {
+
+        $('.sideNav').toggleClass('sideNav1');
+
+    });
+
+    $(document).on('click', '.sideMenu li a', () => {
+
+        $('.sideNav').removeClass('sideNav1');
+
+    });
+
+    $(document).on('click', '.logoSideNav', () => {
+
+
+        $('.sideNav').removeClass('sideNav1');
+
+    });
+
+    // Ispis menija pomocu json-a
+
+    $.ajax({
+
+        url: "assets/json/nav.json",
+        method: "GET",
+        dataType: "json",
+        success: function (data) {
+
+            printMenu(data);
+
+        },
+        error: function (err, msg, type) {
+
+            console.log(msg);
+
+        }
+
+    })
+
+    // Prikaz loadera u zavinsosti od vremena
 
     let time = Math.round(new Date().getTime() / 1000 / 60);
 
@@ -8,17 +49,29 @@ $(document).ready(function () {
 
         if (!localStorage.getItem('videoShow')) {
 
-            $('body').css('overflow-y', "auto");
+            setTimeout(function (e) {
 
-            $('.preloader').fadeOut();
+                $('.infoBlock img').css("animation-delay", "5s");
 
-            $('.infoBlock img').css("animation-delay", "1s");
+                $('.infoBlock a').css("animation-delay", "6s");
 
-            $('.infoBlock a').css("animation-delay", "2s");
+                $('.preloader').fadeOut();
 
-            localStorage.setItem('videoShow', time);
+                $('body').addClass('vert');
 
-            $('.preloader audio').removeAttr('muted');
+                localStorage.setItem('videoShow', time);
+
+            }, 4500);
+
+            // $('body').css('overflow-y', "hidden");
+
+            // $('.preloader').fadeOut();
+
+            // $('.infoBlock img').css("animation-delay", "1s");
+
+            // $('.infoBlock a').css("animation-delay", "2s");
+
+            // $('body').addClass('vert');
 
         }
 
@@ -28,7 +81,7 @@ $(document).ready(function () {
 
             console.log(time - getTimeFromLocal, time, getTimeFromLocal)
 
-            if (time - getTimeFromLocal >= 0) {
+            if (time - getTimeFromLocal >= 5) {
 
                 setTimeout(function (e) {
 
@@ -97,3 +150,41 @@ $(document).ready(function () {
     });
 
 });
+
+// Funkcije
+
+function printMenu(data) {
+
+    let printer = '';
+
+    let allTheMenusCollection = document.getElementsByClassName('menu');
+
+    let allTheMenus = Array.from(allTheMenusCollection);
+
+    for (let el of allTheMenus) {
+
+        for (let link of data) {
+
+            printer += `<li><a href="${link.putanja}">${link.naziv}</a></li>`;
+
+        }
+
+        el.innerHTML = printer;
+
+        printer = '';
+
+    }
+
+}
+
+// Prikaz logoa svuda gde je ta klasa postavljena
+
+let logosCollection = document.getElementsByClassName('allLogos');
+
+let logos = Array.from(logosCollection);
+
+for (let el of logos) {
+
+    el.innerHTML = ` <a href="#!" class='logoImage'><img src="assets/images/milosLogo.png" alt="Logo" class='img-fluid' /></a>`;
+
+}
